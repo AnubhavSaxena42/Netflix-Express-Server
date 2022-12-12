ac653d32c66b25509e2f96ce524d706acfe11b34
AnubhavSaxena43 <anubhavsa@zaamo.co>
2022-12-12 11:20:17 +0530

    feat:authRoute api register

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 const app = express();
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, {})
   .then(() => {
     console.log("DB connected");
   })
   .catch((e) => console.log("DB connection failed", e));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("express running");

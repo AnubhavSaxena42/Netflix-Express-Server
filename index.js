efee605a6f839bf2470f03c684b71d30f3b88409
AnubhavSaxena43 <anubhavsa@zaamo.co>
2022-12-12 10:18:02 +0530

    feat: Mongoose connection and models definition

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((e) => console.log("DB connection failed", e));
 
 app.listen(8800, () => {
   console.log("express running");
